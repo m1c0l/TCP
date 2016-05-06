2ded1e6950e1f7f348e34b1a4eec189fc88bf037
hanqiu8 <hanqiu8@gmail.com>
2016-05-06 22:04:40 +0000

    Get arguments

--- server.cpp
+++ server.cpp
@@ -1,9 +1,23 @@
 #include <string>
 #include <thread>
 #include <iostream>
+#include <sys/socket.h>
 
-int main()
+int main(int argc, char **argv)
 {
-  std::cerr << "web server is not implemented yet" << std::endl;
-  // do your stuff here! or not if you don't want to.
+  if (argc > 3) {
+    cerr << "usage: [PORT-NUMBER] [FILENAME]";
+    return 1;
+  }
+
+  string port, filename;
+
+  //We probably need default values, not sure what they are yet.
+  port = arg[1];
+  filename = arg[2]; 
+
+  //Create UDP socket
+  int sockfd = socket(AF_INET, SOCK_DGRAM,0);
+  
+  return 0;
 }
