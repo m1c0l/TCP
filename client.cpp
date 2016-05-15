9cac0ef7751a9e06b383c0c3e3aad6b38c1f11cd
Richard Sun <rsun@media.ucla.edu>
2016-05-15 07:04:30 +0000

    client

--- client.cpp
+++ client.cpp
@@ -1,16 +1,61 @@
-#include <string>
-#include <thread>
-#include <iostream>
-
-using namespace std;
-
-int main(int argc, char **argv)
+/*
+    Simple udp client
+    Silver Moon (m00n.silv3r@gmail.com)
+*/
+#include<stdio.h> //printf
+#include<string.h> //memset
+#include<stdlib.h> //exit(0);
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include <unistd.h>
+ 
+#define SERVER "10.0.0.1"
+#define BUFLEN 512  //Max length of buffer
+#define PORT 4000   //The port on which to send data
+ 
+void die(char *s)
 {
-	if (argc != 3) {
-		cerr << "usage: ./client SERVER-HOST-OR-IP PORT-NUMBER";
-		return 1;
-	}
-	string hostStr, port;
-	hostStr = argv[1];
-	port = argv[2];
+    perror(s);
+    exit(1);
+}
+ 
+int main(void)
+{
+    struct sockaddr_in si_other;
+    int s;
+    socklen_t slen=sizeof(si_other);
+    char buf[BUFLEN];
+    char message[BUFLEN];
+ 
+    if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+    }
+ 
+    memset((char *) &si_other, 0, sizeof(si_other));
+    si_other.sin_family = AF_INET;
+    si_other.sin_port = htons(PORT);
+     
+    if (inet_aton(SERVER , &si_other.sin_addr) == 0) 
+    {
+    }
+ 
+    strcpy(message, "asdf");
+
+        //send the message
+        if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+        {
+        }
+         
+        //receive a reply and print it
+        //clear the buffer by filling null, it might have previously received data
+        memset(buf,'\0', BUFLEN);
+        //try to receive some data, this is a blocking call
+        if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1)
+        {
+        }
+         
+        puts(buf);
+ 
+    close(s);
+    return 0;
 }
