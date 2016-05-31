03357ef6eeb28fe745a027551db12467c15cef20
Michael Li <michael.li11702@gmail.com>
2016-05-31 01:50:59 -0700

    Write function to increment and mod seq/ack numbers

--- Utils.h
+++ Utils.h
@@ -1,12 +1,20 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <cstdint>
+
 const int BUFFER_SIZE = 1032;
 const int HEADER_SIZE = 8;
 const int DATA_SIZE = BUFFER_SIZE - HEADER_SIZE;
+const uint16_t MAX_SEQ_NUM = 30720;
 
 const uint16_t FIN_FLAG = 1;
 const uint16_t SYN_FLAG = 2;
 const uint16_t ACK_FLAG = 4;
+
+// increment a seq or ack number and mod it by MAX_SEQ_NUM
+uint16_t incSeqNum(uint16_t seq, uint16_t increment) {
+	return (seq + increment) % MAX_SEQ_NUM;
+}
 
 #endif
