eabc9a04873f627a6e897af6d50af3e5eed4c52e
Richard Sun <rsun@media.ucla.edu>
2016-06-03 02:19:20 -0700

    print data size instead of contents

--- TcpMessage.cpp
+++ TcpMessage.cpp
@@ -113,12 +113,12 @@ int TcpMessage::recvfrom(int sockfd, sockaddr_in *si_other, socklen_t len) {
 
 // Print out the TcpMessage's contents
 void TcpMessage::dump() {
-    cout << "seqNum = " << seqNum << '\t';
-    cout << "ackNum = " << ackNum << '\t';
-    cout << "recvWindow = " << recvWindow << endl;
-    cout << "FIN_FLAG = " << getFlag('f') << '\t';
-    cout << "SYN_FLAG = " << getFlag('s') << '\t';
-    cout << "ACK_FLAG = " << getFlag('a') << endl;
-    cout << "data = \"" << data << "\"" << endl;
-    cout << endl;
+	cout << "seqNum = " << seqNum << '\t';
+	cout << "ackNum = " << ackNum << '\t';
+	cout << "recvWindow = " << recvWindow << endl;
+	cout << "FIN_FLAG = " << getFlag('f') << '\t';
+	cout << "SYN_FLAG = " << getFlag('s') << '\t';
+	cout << "ACK_FLAG = " << getFlag('a') << endl;
+	cout << "data.size() = " << data.size() << endl;
+	cout << endl;
 }
